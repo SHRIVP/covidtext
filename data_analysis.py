d821c04f21dca700dbba00160d58c0bc01f0361a
Pushkar <catchpushkar@gmail.com>
2024-09-07 19:58:49 -0400

    Understanding and writing transformer

--- data_analysis.py
+++ data_analysis.py
@@ -41,8 +41,47 @@ batch_size = 4
 def get_batch(split):
     data = train_data if split == 'train' else val_data
     ix = torch.randint(0, len(data) - block_size, (batch_size,))
-    #x = torch.stack([data[i:i+block_size] for i in ix][0])
-    x = torch.stack(torch.tensor)(0)
-    print(ix)
+    x = torch.stack(([torch.tensor(data[i:i+block_size]) for i in ix]))
+    # creates the labels such that if the input is i labels is learning.when the input is i learrning the labels is deep etc.
+    y = torch.stack([torch.tensor(data[i+1:i+block_size+1]) for i in ix])
+    return x, y
 
-get_batch('train')
+xb, yb = get_batch('train')
+print(f' the input for this model:  {xb}')
+print(f' the output matrix for the model: {yb}')
+
+# Now we can create the model  
+
+import torch.nn as nn
+from torch.nn import functional as F
+torch.manual_seed(42)
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super(BigramLanguageModel, self).__init__()
+        # Emebedding table is a 64 by 64 matrix that will be learned.Each row in the table is a token in the tiktoken vocab which is around 100 k in our case.And each token ia vector in 64 dimensions.Each of the 64 dimensions is learning something about that token.
+        self.token_embedding_table = nn.Embedding(tokenizer.n_vocab, vocab_size)
+        
+    def forward(self, x, targets=None):
+        # What we send to miodle is 4 sentences of 8 words each.What we expect from the model is to predict the next word in the sentence.In one pass all the 4 sentences in the batch is processes simultaneously using some kind of weird :) matrix multiplication.The details of how the multplications works is oos for this comment.
+        print(self.token_embedding_table)
+        logits = self.token_embedding_table(x)
+        print(logits)
+        if targets is None:
+            loss = None
+        else:   
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits,targets)
+        return logits, loss
+    
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)
+        return logits
+    
+m = BigramLanguageModel(64)
+logits, loss = m(xb, yb)
+print(logits.shape)
+print(loss)
--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
-Data Analysis of Covid Tweets
+Deep dive of Transformer Model using Covid Tweets
 https://www.kaggle.com/datasets/datatattle/covid-19-nlp-text-classification
+
+Inspired by Andrez Karpathy's https://www.youtube.com/watch?v=kCc8FmEb1nY.
+
