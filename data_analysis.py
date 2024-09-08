9a3f9b53d0a9afbec25dc890108908f6c51a1a5b
Pushkar <catchpushkar@gmail.com>
2024-09-08 16:08:44 -0400

    Understodd Cross Entropy for my model

--- data_analysis.py
+++ data_analysis.py
@@ -60,19 +60,29 @@ class BigramLanguageModel(nn.Module):
     def __init__(self, vocab_size):
         super(BigramLanguageModel, self).__init__()
         # Emebedding table is a 64 by 64 matrix that will be learned.Each row in the table is a token in the tiktoken vocab which is around 100 k in our case.And each token ia vector in 64 dimensions.Each of the 64 dimensions is learning something about that token.
-        self.token_embedding_table = nn.Embedding(tokenizer.n_vocab, vocab_size)
+        self.token_embedding_table = nn.Embedding(tokenizer.n_vocab, 100)
+        self.linear_layer = nn.Linear(100, tokenizer.n_vocab)
         
     def forward(self, x, targets=None):
-        # What we send to miodle is 4 sentences of 8 words each.What we expect from the model is to predict the next word in the sentence.In one pass all the 4 sentences in the batch is processes simultaneously using some kind of weird :) matrix multiplication.The details of how the multplications works is oos for this comment.
+        # What we send to miodle is 4 sentences of 8 words each.What we expect from the model is to predict the next word in the sentence.In one pass all the 4 sentences in the batch is processes simultaneously by looking up the token in the embedding table.
+        #Each token in the embedding table is of 64 dimensions.
         print(self.token_embedding_table)
-        logits = self.token_embedding_table(x)
-        print(logits)
+        embedded_inp = self.token_embedding_table(x)
+        print(f'When we lookup embedding table with the input data we get embedded input with shape {embedded_inp.shape}')
+        logits = self.linear_layer(embedded_inp)
+        print(f'When we apply a matrix multiplication on a table with 4 * 8 x 100 with 100 x 1000k we get an output of shape {logits.shape}')
         if targets is None:
             loss = None
         else:   
             B, T, C = logits.shape
+            print(logits)
             logits = logits.view(B*T, C)
+            print(f' logits after reshape {logits}')
+            # targets = torch.ones(32, dtype=int)*5
             targets = targets.view(B*T)
+            print(targets)
+            # What logits are giving is some scores for 100 k tokens that should appear at this position and targets gas the actuak word at that position
+            # Cross entropy will apply softmax whihc is it will exponentiate all the 100k scores and divide by the sum.This will convert the scores into probs and the word with the highest probabilty becomes the predicted word at that position and then we just compare with the actual target and calculate the loss.
             loss = F.cross_entropy(logits,targets)
         return logits, loss
     
